18ef4e54d5f478f1d1176c9034a0741ab0791dba
Giulianorufus <giuliano.murgia@gmail.com>
2025-12-16 11:00:11 +0100

    refactor(types): extend CadAnalysisMeta (thickness/flowLength/volume)

--- src/types/cadAnalysisMeta.ts
+++ src/types/cadAnalysisMeta.ts
@@ -7,4 +7,8 @@ export type BBoxMM = {
 export type CadAnalysisMeta = {
   bbox_mm?: Partial<BBoxMM>
   projectedArea_cm2?: number
+  thickness_mm?: number
+  flowLength_mm?: number
+  volume_cm3?: number
 }
+
